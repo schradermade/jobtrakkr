54e1bae0440be4cce89e2a51eb9265097241a876
Jerrod Styrk <jstyrk@citadel.edu>
2021-03-02 15:30:55 -0800

    add links to job posting sites
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ class App extends Component {
         "CompanyName" : "Ebay",
         "Position" : "Sales",
         "Location" : "California",
-        "whereWork": "remote"
+        "WhereWork": "remote",
+        "JobLink" : "https://www.ebay.com"
       },
 
       {
@@ -20,7 +21,8 @@ class App extends Component {
         "CompanyName" : "Reddit",
         "Position" : "Moderator",
         "Location" : "Washington",
-        "whereWork": "onsite"
+        "WhereWork": "onsite",
+        "JobLink" : "https://www.reddit.com"
       },
 
       {
@@ -28,7 +30,8 @@ class App extends Component {
         "CompanyName" : "Amazon",
         "Position" : "Sorter",
         "Location" : "Oregon",
-        "whereWork": "remote"
+        "WhereWork": "remote",
+        "JobLink" : "https://www.amazon.com"
       }
     ]
   }
@@ -36,6 +39,12 @@ class App extends Component {
   remove(id) {
     let updatedJobs = [...this.state.jobs].filter (i => i.id !== id)
     this.setState({jobs : updatedJobs });
+  }
+
+  redirect(id) {
+    let job = [...this.state.jobs].filter (i => i.id === id);
+    let url = job[0].JobLink;
+    window.open(url);
   }
 
   render() {
@@ -54,12 +63,13 @@ class App extends Component {
         <td>{job.CompanyName}</td>
         <td>{job.Position}</td>
         <td>{job.Location}</td>
-        { job.whereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> <br/>OnSite</td>}
+        { job.WhereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> <br/>OnSite</td>}
         <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> Accepted </Button></td>
         <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> Denied </Button></td>
         <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> No Response </Button></td>
         <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faSearchDollar} /> ??? </Button></td>
-        <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faFileContract} /> Job Posting </Button></td>
+        <td><Button className='btn btn-lg btn-info' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faFileContract} /> Job Posting </Button></td>
+        
       </tr>
     )
 
