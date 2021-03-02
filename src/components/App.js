cf42f2fca195caab1f41709f58c22c9d7228432a
Jerrod Styrk <jstyrk@citadel.edu>
2021-03-02 14:29:52 -0800

    Add stying and icons to table
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown, faGlobe, faMoneyCheckAlt, faSearchDollar, faFileContract, faBuilding } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
-  state = { 
+  state = {
     isLoading : false,
     jobs : [
       {
         "id" : "1",
         "CompanyName" : "Ebay",
         "Position" : "Sales",
-        "Location" : "California"
+        "Location" : "California",
+        "whereWork": "remote"
       },
 
       {
         "id" : "2",
         "CompanyName" : "Reddit",
         "Position" : "Moderator",
-        "Location" : "Washington"
+        "Location" : "Washington",
+        "whereWork": "onsite"
       },
 
       {
         "id" : "3",
         "CompanyName" : "Amazon",
         "Position" : "Sorter",
-        "Location" : "Oregon"
+        "Location" : "Oregon",
+        "whereWork": "remote"
       }
     ]
+  }
+
+  remove(id) {
+    let updatedJobs = [...this.state.jobs].filter (i => i.id !== id)
+    this.setState({jobs : updatedJobs });
   }
 
   render() {
@@ -38,13 +48,19 @@ class App extends Component {
       );
     }
 
-    let jobs = allJobs.map(job => {
+    let jobs = 
+    allJobs.map(job => 
       <tr key={job.Id}>
         <td>{job.CompanyName}</td>
         <td>{job.Position}</td>
         <td>{job.Location}</td>
+        { job.whereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> <br/>OnSite</td>}
+        <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> Accepted </Button></td>
+        <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> Denied </Button></td>
+        <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> No Response </Button></td>
+        <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faSearchDollar} /> ??? </Button></td>
+        <td><Button className='btn btn-lg btn-info' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faFileContract} /> Job Posting </Button></td>
       </tr>
-    }
     )
 
     return ( 
@@ -61,11 +77,13 @@ class App extends Component {
                 <th>Company Name</th>
                 <th>Position</th>
                 <th>Location</th>
+                <th>Work From</th>
                 <th colSpan='4'>Actions</th>
+                <th>Job Posting</th>
               </thead>
 
               <tbody>
-                { this.state.jobs.length === 0 ? <td colSpan='7'>No pending Applications.</td> : jobs}
+                { this.state.jobs.length === 0 ? <td colSpan='8'>No pending Applications.</td> : jobs}
               </tbody>
             </Table>
           </div>
