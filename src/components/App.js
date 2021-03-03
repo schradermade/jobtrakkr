12ea62f5b08dc883f6170219f2ccbce38000b1cb
Jerrod Styrk <jstyrk@citadel.edu>
2021-03-03 13:25:48 -0800

    Create third table and move jobs between them
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>

--- src/components/App.js
+++ src/components/App.js
@@ -10,31 +10,37 @@ class App extends Component {
       {
         "id" : "1",
         "CompanyName" : "Ebay",
-        "Position" : "Sales",
+        "JobTitle" : "Sales",
         "Location" : "California",
         "WhereWork": "remote",
         "JobLink" : "https://www.ebay.com",
-        "Status" : false
+        "CoverLetterLink" : "https://docs.google.com/document/d/1kreMCCgfnh947-n8DB29ExRcF3MeL29ZCQmodDn3ltk/edit?usp=sharing",
+        "ResumeLink" : "https://www.google.com",
+        "Status" : "pending"
       },
 
       {
         "id" : "2",
         "CompanyName" : "Reddit",
-        "Position" : "Moderator",
+        "JobTitle" : "Moderator",
         "Location" : "Washington",
         "WhereWork": "onsite",
         "JobLink" : "https://www.reddit.com",
-        "Status" : false
+        "CoverLetterLink" : "https://docs.google.com/document/d/1kreMCCgfnh947-n8DB29ExRcF3MeL29ZCQmodDn3ltk/edit?usp=sharing",
+        "ResumeLink" : "https://www.google.com",
+        "Status" : "pending"
       },
 
       {
         "id" : "3",
         "CompanyName" : "Amazon",
-        "Position" : "Sorter",
+        "JobTitle" : "Sorter",
         "Location" : "Oregon",
         "WhereWork": "remote",
         "JobLink" : "https://www.amazon.com",
-        "Status" : false
+        "CoverLetterLink" : "https://docs.google.com/document/d/1kreMCCgfnh947-n8DB29ExRcF3MeL29ZCQmodDn3ltk/edit?usp=sharing",
+        "ResumeLink" : "https://www.google.com",
+        "Status" : "pending"
       }
     ]
   }
@@ -50,9 +56,28 @@ class App extends Component {
     window.open(url);
   }
 
+  redirectCoverLetter(id) {
+    let job = [...this.state.jobs].filter (i => i.id === id);
+    let url = job[0].CoverLetterLink;
+    window.open(url);
+  }
+
+  redirectResume(id) {
+    let job = [...this.state.jobs].filter (i => i.id === id);
+    let url = job[0].ResumeLink;
+    window.open(url);
+  }
+
   firstRound(id) {
     let job = this.state.jobs.filter (i => i.id === id);
-    job[0].Status = true;
+    job[0].Status = "firstRound";
+    let updatedJobs = [...this.state.jobs];
+    this.setState({jobs : updatedJobs });
+  }
+
+  secondRound(id) {
+    let job = this.state.jobs.filter (i => i.id === id);
+    job[0].Status = "secondRound";
     let updatedJobs = [...this.state.jobs];
     this.setState({jobs : updatedJobs });
   }
@@ -60,7 +85,8 @@ class App extends Component {
   render() {
     const isLoading = this.state.isLoading;
     const allJobs = this.state.jobs;
-    const pendingJobs = this.state.jobs;
+    const firstRoundJobs = this.state.jobs;
+    const secondRoundJobs = this.state.jobs;
   
     if (isLoading) {
       return(
@@ -70,33 +96,53 @@ class App extends Component {
 
     let jobs = 
     allJobs.map(job => {
-      if (job.Status === false) {
+      if (job.Status === "pending") {
         return <tr key={job.Id}>
                   <td>{job.CompanyName}</td>
-                  <td>{job.Position}</td>
+                  <td>{job.JobTitle}</td>
                   <td>{job.Location}</td>
                   { job.WhereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> <br/>OnSite</td>}
-                  <td><Button className='btn btn-lg btn-success' onClick={() => this.firstRound(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> <br/>Move to First Round </Button></td>
-                  <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> Denied </Button></td>
-                  <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> No Response </Button></td>
-                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faFileContract} /> Job Posting </Button></td>
+                  <td><Button className='btn btn-lg btn-success' onClick={() => this.firstRound(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> <br/></Button></td>
+                  <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-warning' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectResume(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectCoverLetter(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
                 </tr>
         }
       }
     )
     
-    let firstJobs = 
-    pendingJobs.map(job => {
-      if (job.Status === true) {
+    let jobs2 = 
+    firstRoundJobs.map(job => {
+      if (job.Status === "firstRound") {
         return <tr key={job.Id}>
                   <td>{job.CompanyName}</td>
-                  <td>{job.Position}</td>
+                  <td>{job.JobTitle}</td>
                   <td>{job.Location}</td>
                   { job.WhereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> OnSite</td>}
-                  <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> <br/>Move to Second Round </Button></td>
-                  <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> Denied </Button></td>
-                  <td><Button className='btn btn-lg btn-warning' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> No Response </Button></td>
-                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faFileContract} /> Job Posting </Button></td>
+                  <td><Button className='btn btn-lg btn-success' onClick={() => this.secondRound(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /> <br/> </Button></td>
+                  <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-warning' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectResume(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectCoverLetter(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
+                </tr>
+        }
+      }
+    )
+
+    let jobs3 = 
+    secondRoundJobs.map(job => {
+      if (job.Status === "secondRound") {
+        return <tr key={job.Id}>
+                  <td>{job.CompanyName}</td>
+                  <td>{job.JobTitle}</td>
+                  <td>{job.Location}</td>
+                  { job.WhereWork === "remote" ? <td><FontAwesomeIcon icon={faGlobe} /> Remote</td> : <td><FontAwesomeIcon icon={faBuilding} /> OnSite </td>}
+                  <td><Button className='btn btn-lg btn-success' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsUp} /><br/></Button></td>
+                  <td><Button className='btn btn-lg btn-danger' onClick={() => this.remove(job.id)}> <FontAwesomeIcon icon={faThumbsDown} /></Button></td>
+                  <td><Button className='btn btn-lg btn-warning' onClick={() => this.redirect(job.id)}> <FontAwesomeIcon icon={faMoneyCheckAlt} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectResume(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
+                  <td><Button className='btn btn-lg btn-info' onClick={() => this.redirectCoverLetter(job.id)}> <FontAwesomeIcon icon={faFileContract} /> </Button></td>
                 </tr>
         }
       }
@@ -116,11 +162,14 @@ class App extends Component {
                 <thead>
                   <tr>
                     <th>Company Name</th>
-                    <th>Position</th>
+                    <th>Job Title</th>
                     <th>Location</th>
                     <th>Work From</th>
-                    <th colSpan='3'>Actions</th>
+                    <th>Advance<br/> First Round</th>
+                    <th>Denial</th>
                     <th>Job Posting</th>
+                    <th>Resume</th>
+                    <th>Cover<br/> Letter URL</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -130,11 +179,13 @@ class App extends Component {
             </div>
           </div>
         </div>
+
         <hr/>
+
         <div className='container border border-secondary rounded center'>
           <div className='row'>
             <div className='col-12'>
-              <h4>Accepted for Interview Process</h4>
+              <h4>Accepted for First Round</h4>
             </div>
           </div>
           <div className='row'>
@@ -143,15 +194,50 @@ class App extends Component {
                 <thead>
                   <tr>
                     <th>Company Name</th>
-                    <th>Position</th>
+                    <th>Job Title</th>
                     <th>Location</th>
                     <th>Work From</th>
-                    <th colSpan='3'>Actions</th>
+                    <th>Advance<br/> Second Round</th>
+                    <th>Denial</th>
                     <th>Job Posting</th>
+                    <th>Resume</th>
+                    <th>Cover<br/>Letter URL</th>
                   </tr>
                 </thead>
                 <tbody>
-                  { this.state.jobs.length === 0 ? <td colSpan='8'>No pending Applications.</td> : firstJobs}
+                  { this.state.jobs.length === 0 ? <td colSpan='8'>No pending Applications.</td> : jobs2}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <div className='container border border-secondary rounded center'>
+          <div className='row'>
+            <div className='col-12'>
+              <h4>Accepted for Second Round</h4>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-xs-12 center text-center'>
+              <Table dark responsive striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Company Name</th>
+                    <th>Job Title</th>
+                    <th>Location</th>
+                    <th>Work From</th>
+                    <th>Advance<br/> Third Round</th>
+                    <th>Denial</th>
+                    <th>Job Posting</th>
+                    <th>Resume</th>
+                    <th>Cover<br/>Letter URL</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { this.state.jobs.length === 0 ? <td colSpan='8'>No pending Applications.</td> : jobs3}
                 </tbody>
               </Table>
             </div>
